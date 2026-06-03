abfa075e6477f45580eb12d17a8b48e40792d0d2
Manuel-666 <chamuel2006@gmail.com>
2026-06-03 18:27:03 -0500

    feat: agregar estadisticas al perfil

--- .adonisjs/server/controllers.ts
+++ .adonisjs/server/controllers.ts
@@ -33,6 +33,7 @@ export const controllers = {
     RecuperarPasswords: () => import('#controllers/auth/recuperar_passwords_controller'),
     Registros: () => import('#controllers/auth/registros_controller'),
   },
+  Chatbot: () => import('#controllers/chatbot_controller'),
   Deteccion: () => import('#controllers/deteccion_controller'),
   encargado: {
     Entregas: () => import('#controllers/encargado/entregas_controller'),
@@ -53,5 +54,4 @@ export const controllers = {
     ReservasUsuario: () => import('#controllers/usuario/reservas_usuario_controller'),
     ReservasUsuarios: () => import('#controllers/usuario/reservas_usuarios_controller'),
   },
-  Chatbot: () => import('#controllers/chatbot_controller'),
 }
