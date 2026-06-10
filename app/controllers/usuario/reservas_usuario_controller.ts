0c62e673e71bc002f29acb28c481f09ece84de50
Angie Escobar <escobarav103@gmail.com>
2026-06-09 22:40:43 -0500

    Corregir creación de notificaciones de reservas

--- app/controllers/usuario/reservas_usuario_controller.ts
+++ app/controllers/usuario/reservas_usuario_controller.ts
@@ -86,12 +86,13 @@ export default class ReservasUsuarioController {
       console.log("📤 Voy a emitir socket al encargado", encargado.idUsuario)
       await Notificacion.create({
         idUsuario: encargado.idUsuario,
+        idEncargado: encargado.idUsuario,
+        tipo: 'nueva_reserva',
         titulo: 'Nueva reserva',
-        mensaje: `El usuario ${auth.user!.nombre} ha reservado en ${punto.nombre} para el ${fecha} a las ${hora}.`,
+        descripcion: `El usuario ${auth.user!.nombre} ha reservado en ${punto.nombre} para el ${fecha} a las ${hora}.`,
         leida: false,
-        tipo: 'reserva',
-        idReferencia: reserva.idReserva,
       })
+      // idReferencia: reserva.idReserva,
 
       WsService.emitToEncargado(encargado.idUsuario, 'notificacion', {
         tipo: 'nueva_reserva',
